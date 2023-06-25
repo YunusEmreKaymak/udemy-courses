d8c5be623517c2b8d64cdd3366a290dff1ae088f
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-06-25 15:33:52 +0300

    navbar and album components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Album from './components/main/main-page';
+import { NavBar } from './components/navbar/navbar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
+  const defaultTheme = createTheme();
   return (
     <div className="App">
-
+      <ThemeProvider theme={defaultTheme}>
+        <NavBar />
+        <Album />
+      </ThemeProvider>
     </div>
   );
 }

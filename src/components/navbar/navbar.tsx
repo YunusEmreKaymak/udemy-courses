502d0780ce05131ae04c09276870e21487f155cf
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-06-25 15:31:50 +0300

    navbar created and menu and searchbar components added

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-export const Navbar = () => {
+import AppBar from '@mui/material/AppBar';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import PositionedMenu from './menu';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { TextField } from '@mui/material';
+import { Searchbar } from './searchbar';
+export const NavBar = () => {
   return (
-    <div>
-        
-    </div>
+    <AppBar position="relative">
+      <Toolbar >
+            <PositionedMenu />
+            <Searchbar />
+      </Toolbar>
+    </AppBar>
   )
 }

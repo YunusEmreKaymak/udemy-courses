1cbcb59e8c3d2a8eaa638ff87a6fefdca7491fb9
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-05 21:08:03 +0300

    Router added

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import './App.css';
 import Album from './components/main/main-page';
 import { NavBar } from './components/navbar/navbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddCourse from './components/addCourse/AddCourse';
 function App() {
   const defaultTheme = createTheme();
   return (
     <div className="App">
+
       <ThemeProvider theme={defaultTheme}>
+        <Router>
         <NavBar />
-        <Album />
+        <Routes>
+          <Route path='/' element={<Album />}></Route> :
+          <Route path='/addCourse' element={<AddCourse />}></Route>
+        </Routes>
+      </Router>
       </ThemeProvider>
     </div>
   );

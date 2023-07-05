6eee881da7b244b1c9e373ce968a65e9a16cee3c
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-05 21:12:19 +0300

    unused comment deleted

--- src/components/navbar/searchbar.tsx
+++ src/components/navbar/searchbar.tsx
@@ -16,7 +16,6 @@ export const Searchbar = () => {
         <TextField fullWidth label="Search for a course" id="fullWidth" color='primary' style={{
           borderRadius: 35,
           backgroundColor: "#f3e5f5",
-          // padding: "18px 36px",
           fontSize: "18px",
           width: '1200px',
           marginLeft: '15px'

57f9abcac33cb904c69b0b2cc5821ea352889b33
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-08 15:19:14 +0300

    category path added to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
-import Album from './components/main/main-page';
+import Course from './components/main/main-page';
 import { NavBar } from './components/navbar/navbar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddCourse from './components/addCourse/AddCourse';
+import Category from './components/main/main-page-category';
 function App() {
   const defaultTheme = createTheme();
   return (
     <div className="App">
-
       <ThemeProvider theme={defaultTheme}>
         <Router>
         <NavBar />
         <Routes>
-          <Route path='/' element={<Album />}></Route> :
+          <Route path='/' element={<Course />}></Route> :
+          <Route path='/category' element={<Category />}></Route> :
           <Route path='/addCourse' element={<AddCourse />}></Route>
         </Routes>
       </Router>

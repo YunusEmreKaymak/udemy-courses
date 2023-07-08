07524f320c602b01d50e5f9565a7c24ef45120a5
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-08 14:39:05 +0300

    style added for add-course-button

--- src/components/navbar/add-course-button.tsx
+++ src/components/navbar/add-course-button.tsx
@@ -15,8 +15,20 @@ export const AddCourseButton = () => {
   return (
     <div>
       {window.location.href === "http://localhost:3000/addCourse" ?
-        <button onClick={handleClick} >Main Page</button> :
-        <button onClick={handleClick} >Add Course</button>}
+        <button onClick={handleClick} style={{
+          borderRadius: 35,
+          backgroundColor: "#f3e5f5",
+          padding: "18px 36px",
+          fontSize: "18px",
+          marginLeft: '15px'
+        }}>Main Page</button> :
+        <button onClick={handleClick} style={{
+          borderRadius: 35,
+          backgroundColor: "#f3e5f5",
+          padding: "18px 36px",
+          fontSize: "18px",
+          marginLeft: '15px'
+        }}>Add Course</button>}
     </div>
   )
 }

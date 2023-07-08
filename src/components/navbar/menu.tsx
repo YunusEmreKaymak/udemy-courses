df529b686f71fc09650089be8c3ca322af6795be
YunusEmreKaymak <y.e.imp@hotmail.com>
2023-07-08 15:18:37 +0300

    menu click navigated

--- src/components/navbar/menu.tsx
+++ src/components/navbar/menu.tsx
@@ -3,17 +3,19 @@ import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
-import { ThemeProvider, createTheme } from '@mui/material/styles'; 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { purple } from '@mui/material/colors';
+import { useNavigate } from 'react-router';
 export default function PositionedMenu() {
+  const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClose = (props: number) => {
-    console.log(props.toString());
+  const handleClose = (props: string) => {
     setAnchorEl(null);
+    navigate("/category",{state:props})
   };
   const purpleTheme = createTheme({ palette: { primary: purple } })
 
@@ -34,8 +36,8 @@ export default function PositionedMenu() {
             backgroundColor: "#f3e5f5",
             padding: "18px 36px",
             fontSize: "18px",
-        }}
-          // sx={{ border: 5, borderColor: "secondary.main" }}
+          }}
+        // sx={{ border: 5, borderColor: "secondary.main" }}
         >
           Categories
         </Button>
@@ -56,14 +58,14 @@ export default function PositionedMenu() {
           horizontal: 'left',
         }}
       >
-        <MenuItem onClick={() => handleClose(1)}>Development</MenuItem>
-        <MenuItem onClick={() => handleClose(2)}>Business</MenuItem>
-        <MenuItem onClick={() => handleClose(3)}>Finance</MenuItem>
-        <MenuItem onClick={() => handleClose(4)}>IT</MenuItem>
-        <MenuItem onClick={() => handleClose(5)}>Office</MenuItem>
-        <MenuItem onClick={() => handleClose(6)}>Personal</MenuItem>
-        <MenuItem onClick={() => handleClose(7)}>Design</MenuItem>
-        <MenuItem onClick={() => handleClose(8)}>Marketing</MenuItem>
+        <MenuItem onClick={() => handleClose("Development")}>Development</MenuItem>
+        <MenuItem onClick={() => handleClose("Business")}>Business</MenuItem>
+        <MenuItem onClick={() => handleClose("Finance")}>Finance</MenuItem>
+        <MenuItem onClick={() => handleClose("IT")}>IT</MenuItem>
+        <MenuItem onClick={() => handleClose("Office")}>Office</MenuItem>
+        <MenuItem onClick={() => handleClose("Personal")}>Personal</MenuItem>
+        <MenuItem onClick={() => handleClose("Design")}>Design</MenuItem>
+        <MenuItem onClick={() => handleClose("Marketing")}>Marketing</MenuItem>
       </Menu>
     </div>
   );
